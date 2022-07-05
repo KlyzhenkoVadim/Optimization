d20f67b60623e980f577b1798d1a86f623c3e9f9
Klyzhenko, Vadim <klyzhenko.vs@cet-mipt.ru>
2022-07-05 19:29:09 +0300

    start testing CurveHold

--- Optimization/Source.cpp
+++ Optimization/Source.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 #include "TrajectoryTemplate.h"
-
+#include "CurveHold.h"
+#include <fstream>
 class Calculator {
 public:
 	virtual void calc() = 0;
@@ -21,6 +22,7 @@ public:
 	}
 };
 
+void writeData(std::vector<Eigen::Vector3d>& pointsCartesian, std::string filename);
 
 int main()
 {
@@ -92,8 +94,24 @@ int main()
 	calcs[0]->calc();
 	calcs[1]->calc();
 
-
+	Eigen::Vector3d pInitial{ 0,0,0 };
+	double theta = 0;
+	double phi = 0;
+	Eigen::Vector3d pTarget{ 100,100,500 };
+	double R = 100;
+	CurveHold ch1 = CurveHold(pInitial, pTarget, theta, phi, R);
+	ch1.fit();
+	ch1.points(CoordinateSystem::CARTESIAN);
+	writeData(ch1.pointsCartesian, "output.txt");
 
 	return 0;
 }
 
+void writeData(std::vector<Eigen::Vector3d>& pointsCartesian, std::string filename) {
+	std::ofstream output;
+	output.open(filename);
+	for (size_t i = 0; i < pointsCartesian.size(); ++i) {
+		output << pointsCartesian[i][0] << "," << pointsCartesian[i][1] << "," << pointsCartesian[i][2] << std::endl;
+	}
+	output.close();
+}
