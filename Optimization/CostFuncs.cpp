40883f91d2b2fe9fcfa5f6f33e300b483673d84d
Klyzhenko, Vadim <klyzhenko.vs@cet-mipt.ru>
2022-07-06 18:28:16 +0300

    ddi,sepF works correct

--- Optimization/CostFuncs.cpp
+++ Optimization/CostFuncs.cpp
@@ -5,56 +5,56 @@ double sigmoid(double x, double penalty, double alpha, double x0) {
 	return penalty / (1 + exp(alpha * (x - x0)));
 }
 
-double signum(double x) {
-	if (x > 0.) {
-		return +1;
-	}
-	if (x < 0.) {
+int signum(double x) {
+	if (x > 0)
+		return 1;
+	if (x < 0)
 		return -1;
-	}
-	if (x < 1e-5) {
-		return 0;
-	}
+	return 0;
 }
 
 double sepFactor(std::vector<Eigen::Vector3d>& pCartesianW1,
 	std::vector<Eigen::Vector4d>& pMDW1,
 	std::vector<Eigen::Vector3d>& pCartesianW2, bool actFunc = true, double penalty = 100) {
 	size_t n = pCartesianW1.size(), m = pCartesianW2.size();
-	std::vector<std::vector<double>> distanceMatrix;
-	std::vector<std::vector<double>> scalarProdMatrix;
+	Eigen::MatrixXd distanceMatrix(n, m);//std::vector<std::vector<double>> distanceMatrix;
+	Eigen::MatrixXd scalarProdMatrix(n, m); //std::vector<std::vector<double>> scalarProdMatrix;
 	double sgm = 4 * 5e-3;
 	double eps = 1e-6;
 	for (size_t i = 0; i < n; ++i) {
-		scalarProdMatrix[i][0] = -1.;
+		scalarProdMatrix(i,0) = -1.;
 		for (size_t j = 0; j < m; ++j) {
-			distanceMatrix[i][j] = 1e6;
+			distanceMatrix(i,j) = 1e3;
 		}
 	}	
 	size_t k = 2;
 	for (size_t idxN = 1; idxN < n; ++idxN) {
+		bool flg = true;
 		for (size_t idxM = k - 1; idxM < m; ++idxM) {
 			Eigen::Vector3d diffVector = pCartesianW2[idxM] - pCartesianW1[idxN];
-			Eigen::Vector3d tangentW1 = {pMDW1[idxN][1], pMDW1[idxN][2], pMDW1[idxN][3]};
+			Eigen::Vector3d tangentW1 = { pMDW1[idxN][1], pMDW1[idxN][2], pMDW1[idxN][3] };
 			if (tangentW1.norm() < eps or diffVector.norm() < eps) {
-				scalarProdMatrix[idxN][idxM] = 0.;
+				scalarProdMatrix(idxN, idxM) = 0.;
 			}
 			else {
-				scalarProdMatrix[idxN][idxM] = round(tangentW1.dot(diffVector) / tangentW1.norm() / diffVector.norm()*100000)/100000;
+				scalarProdMatrix(idxN, idxM) = round(tangentW1.dot(diffVector) / tangentW1.norm() / diffVector.norm() * 100000) / 100000;
 			}
-			if (signum(scalarProdMatrix[idxN][idxM]) != signum(scalarProdMatrix[idxN][idxM - 1])) {
-				distanceMatrix[idxN][idxM] = diffVector.norm() / sgm / pMDW1[idxN][0];
-			}
-		}
-		double sepFactor = 1e3;
-		for (size_t i = 0; i < n; i++){
-			for (size_t j = 0; j < m; ++j) {
-				if (sepFactor > distanceMatrix[i][j]) {
-					sepFactor = distanceMatrix[i][j];
+			if ((signum(scalarProdMatrix(idxN, idxM)) != signum(scalarProdMatrix(idxN, idxM - 1)) and idxM > 1)) {
+				distanceMatrix(idxN, idxM) = diffVector.norm() / (sgm * pMDW1[idxN][0]);
+				if (flg) {
+					k = idxM;
+					flg = false;
 				}
-				
 			}
 		}
+	}
+		double sepFactor = 1e3;
+		for (size_t i = 0; i < n; ++i){
+			for (size_t j = 0; j < m; ++j) {
+				sepFactor = std::min(distanceMatrix(i, j), sepFactor);
+			}
+		}
+
 		if (actFunc) {
 			return sigmoid(sepFactor, penalty, 10, 1.5);
 		}
@@ -62,7 +62,7 @@ double sepFactor(std::vector<Eigen::Vector3d>& pCartesianW1,
 	}
 	
 	// ...
-}
+
 
 double AHD(std::vector<double>& pX, std::vector<double>& pY) {
 	double AHD = 0;
@@ -80,15 +80,19 @@ double dls(Eigen::Vector3d& tangent1, Eigen::Vector3d& tangent2) {
 	if (tangent1.norm() < EPSILON or tangent2.norm() < EPSILON) {
 		return 0.;
 	}
-	double angle = tangent1.dot(tangent2) / tangent1.norm() / tangent2.norm();
-	if (angle + 1 < EPSILON or angle - 1 > EPSILON) {
+	double dotProd= tangent1.dot(tangent2) / tangent1.norm() / tangent2.norm();
+	if (dotProd + 1 < EPSILON or dotProd - 1 > EPSILON) {
 		return 0.;
 	}
-	return (180 / PI) * acos(angle);
+	if (fabs(dotProd - 1.) < EPSILON)
+		dotProd = 1.;
+	if (fabs(dotProd + 1) < EPSILON)
+		dotProd = -1.;
+	return (180. / PI) * acos(dotProd);
 }
 
 double Tortuosity(std::vector<Eigen::Vector4d>& pointsMD) {
-	double tortuos = 0;
+	double tortuos = 0.;
 	for (size_t i = 1; i < pointsMD.size(); ++i) {
 		Eigen::Vector3d tPresent = { pointsMD[i][1],pointsMD[i][2],pointsMD[i][3]};
 		Eigen::Vector3d tPrev = { pointsMD[i-1][1],pointsMD[i-1][2],pointsMD[i-1][3]};
@@ -102,8 +106,8 @@ double DDI(std::vector<Eigen::Vector3d>& pCartesian,std::vector<Eigen::Vector4d>
 	double DDI;
 	std::vector<double> pX, pY;
 	for (size_t idx = 0; idx < pCartesian.size(); ++idx) {
-		pX[idx] = pCartesian[idx][0];
-		pY[idx] = pCartesian[idx][1];
+		pX.push_back(pCartesian[idx][0]);
+		pY.push_back(pCartesian[idx][1]);
 	}
 	double ahd = AHD(pX,pY);
 	double MD = pMD.back()[0] - pMD[0][0];
@@ -112,6 +116,7 @@ double DDI(std::vector<Eigen::Vector3d>& pCartesian,std::vector<Eigen::Vector4d>
 	if (actFunc) {
 		return sigmoid(DDI, penalty, -30, 6.4);
 	}
+	return DDI;
 }	
 
 
--- Optimization/Source.cpp
+++ Optimization/Source.cpp
@@ -4,6 +4,7 @@
 #include "TrajectoryTemplate.h"
 #include "CurveHold.h"
 #include <fstream>
+#include "CostFuncs.h"
 class Calculator {
 public:
 	virtual void calc() = 0;
@@ -44,10 +45,26 @@ int main()
 	double phi = 0;
 	Eigen::Vector3d pTarget{ 100,100,500 };
 	double R = 100;
-	CurveHold ch1 = CurveHold(pInitial, pTarget, theta, phi, R);
-	ch1.fit();
-	ch1.points(CoordinateSystem::CARTESIAN);
-	writeData(ch1.pointsCartesian, "output.txt");
+	TrajectoryTemplate * ch1 = new CurveHold(pInitial, pTarget, theta, phi, R);
+	ch1->fit();
+	ch1->points(CoordinateSystem::CARTESIAN);
+	ch1->points(CoordinateSystem::MD);
+	//writeData(ch1->pointsCartesian, "output.txt");
+
+	Eigen::Vector3d pInitial2{ 0,15,0 };
+	Eigen::Vector3d pTarget2{ -100,-100,500 };
+	TrajectoryTemplate* ch2 = new CurveHold(pInitial2, pTarget2, 0, 0, 100);
+	ch2->fit();
+	ch2->points(CoordinateSystem::CARTESIAN);
+	ch2->points(CoordinateSystem::MD);
+	double ch1DDI = DDI(ch1->pointsCartesian, ch1->pointsMd, false, 10);
+	std::cout << "DDI is: " << ch1DDI << std::endl;
+	std::vector<TrajectoryTemplate*> tT;
+	tT.push_back(ch1);
+	std::cout << "allLength: " << allLength(tT) << std::endl;
+	
+	double sf = sepFactor(ch1->pointsCartesian, ch1->pointsMd, ch2->pointsCartesian, false, 100);
+	std::cout << "sepFactor: " << sf << std::endl;
 
 	return 0;
 }
